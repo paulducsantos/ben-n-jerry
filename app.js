d4dbce219f4e3ef79e62deab337beb1d5e734cab
Paul Santos <paulmichael.a.santos@gmail.com>
2016-02-08 21:08:35 -0500

    renamed layout file

--- app.js
+++ app.js
@@ -10,7 +10,15 @@ app.set('view engine', 'handlebars');
 var icecreams = [ {name: 'vanilla', price: 10, awesomeness: 3}, {name: 'chocolate', price: 4, awesomeness: 8}, {name: 'banana', price: 1, awesomeness: 1}, {name: 'greentea', price: 5, awesomeness: 7}, {name: 'jawbreakers', price: 6, awesomeness: 2}, ]; 
 
 app.get('/', function (req, res) {
-    res.render('home');
+    res.send('ICE CREAM!');
+    // res.render('home');
+});
+
+app.get('/:icecream', function (req, res) {
+
+    var data = icecreams[0];
+    res.render('flavor', data);
+    // res.render('home');
 });
  
 app.listen(PORT, function() {
